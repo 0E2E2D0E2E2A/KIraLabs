17049d6d3d58e4dd78cebe8ce074fdc3777a20e4
Никита Кот <nkot2340@gmail.com>
2023-04-25 21:17:08 +0300

    efwf

---
+++ ConsoleApp12/Program.cs
@@ -0,0 +1,60 @@
+﻿namespace ConsoleApp12
+{
+    class DiscriminantLessThanZero : Exception
+    {
+        protected Exception e;
+        private string m;
+
+        public DiscriminantLessThanZero() { }
+
+        public DiscriminantLessThanZero(string m) : base(m)
+        {
+
+        }
+        public DiscriminantLessThanZero(string m, Exception i) : base(m, i)
+        {
+            m = m;
+            i = e;
+        }
+    }
+
+    class TryException : DiscriminantLessThanZero
+    {
+        public TryException()
+        {
+            Random randomA = new Random();
+            Random randomB = new Random();
+            Random randomC = new Random();
+
+            int a, b, c;
+
+            a = randomA.Next(-30, 30);
+            b = randomB.Next(-30, 30);
+            c = randomC.Next(-30, 30);
+
+            double D = Math.Pow(b, 2) - 4 * a * c;
+
+
+            if (D < 0)
+            {
+                throw new DiscriminantLessThanZero($"Дискриминант меньше нуля", e);
+            }
+
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                TryException exception = new TryException();
+            }
+            catch (DiscriminantLessThanZero exception)
+            {
+                Console.WriteLine(exception.Message);
+            }
+        }
+    }
+}
